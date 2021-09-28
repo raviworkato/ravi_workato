d46bff30a882f88b74844576b7661338bd14b0d8
raviworkato <83234403+raviworkato@users.noreply.github.com>
2021-09-29 03:11:29 +0530

    Initial commit

---
+++ models/integration/int_ad_campaigns.sql
@@ -0,0 +1,38 @@
+{% if var('marketing_warehouse_ad_campaign_sources') %}
+
+with campaigns as
+  (
+    {% for source in var('marketing_warehouse_ad_campaign_sources') %}
+      {% set relation_source = 'stg_' + source + '_campaigns' %}
+
+      select
+        '{{source}}' as source,
+        *
+        from {{ ref(relation_source) }}
+
+        {% if not loop.last %}union all{% endif %}
+      {% endfor %}
+  )
+select *,
+
+       case when ad_network = 'Google Ads' then 'adwords'
+            when ad_network = 'Facebook Ads' then 'facebook'
+            when ad_network = 'Hubspot Email' then 'hs_email'
+            end as utm_source,
+       case when ad_network = 'Google Ads' then 'ppc'
+            when ad_network = 'Facebook Ads' then 'paid_social'
+            when ad_network in ('Mailchimp','Hubspot Email') then 'email'
+            else null end as utm_medium,
+       lower(ad_campaign_name) as utm_campaign
+ from campaigns
+
+ {% else %}
+
+ {{
+     config(
+         enabled=false
+     )
+ }}
+
+
+ {% endif %}
